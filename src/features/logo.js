5cfe2c678e6e71ef8c5fc18893b2384bbf116d44
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2024-08-01 13:20:44 +0200

    fix: testing

--- src/features/logo.js
+++ src/features/logo.js
@@ -1,21 +1,22 @@
 function animateLogo() {
 
-  const playerContainers = document.querySelectorAll(".lottie-animation");
+  // const playerContainers = document.querySelectorAll(".lottie-animation");
 
-  playerContainers.forEach(container => {
-    const player = container.querySelector("dotlottie-player")
+  // playerContainers.forEach(container => {
+  //   const player = container.querySelector("dotlottie-player")
 
 
-    container.addEventListener("mouseover", () => {
-      player.setDirection(1);
-      player.play();
-    });
+  //   container.addEventListener("mouseover", () => {
+  //     player.setDirection(1);
+  //     player.play();
+  //   });
 
-    container.addEventListener("mouseleave", () => {
-      player.setDirection(-1);
-      player.play();
-    });
-  });
+  //   container.addEventListener("mouseleave", () => {
+  //     player.setDirection(-1);
+  //     player.play();
+  //   });
+  // });
+  console.log("animate logo");
 }
 
 export default animateLogo
