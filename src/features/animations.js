5d9d0ee9ab2c290c55d99a1f0498d3d97d1fada0
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2024-09-17 16:15:35 +0200

    fix: removes hover states on touch devices

--- src/features/animations.js
+++ src/features/animations.js
@@ -1,20 +1,24 @@
-function animateContentMenu() {
-  document.querySelectorAll('.link--content').forEach(element => {
-    element.addEventListener('mouseenter', () => {
-      console.log('hover');
-      document.querySelectorAll('.link--content').forEach(sibling => {
-        if (sibling !== element) {
-          sibling.classList.add('dimmed');
-        }
-      });
-    });
+const isTouchDevice = window.matchMedia("(hover: none) and (pointer: coarse)").matches;
 
-    element.addEventListener('mouseleave', () => {
-      document.querySelectorAll('.link--content').forEach(sibling => {
-        sibling.classList.remove('dimmed');
+function animateContentMenu() {
+  if (!isTouchDevice) {
+    document.querySelectorAll('.link--content').forEach(element => {
+      element.addEventListener('mouseenter', () => {
+        console.log('hover');
+        document.querySelectorAll('.link--content').forEach(sibling => {
+          if (sibling !== element) {
+            sibling.classList.add('dimmed');
+          }
+        });
+      });
+
+      element.addEventListener('mouseleave', () => {
+        document.querySelectorAll('.link--content').forEach(sibling => {
+          sibling.classList.remove('dimmed');
+        });
       });
     });
-  });
+  }
 }
 
 export default animateContentMenu;
