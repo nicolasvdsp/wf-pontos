17612a2d054071592380f35b096e945883df80ca
Nicolas van der Straten Ponthoz <nicolasvds@live.be>
2024-09-17 19:58:39 +0200

    feat: animates logo every 10s on mobile

--- src/features/logo.js
+++ src/features/logo.js
@@ -1,4 +1,4 @@
-function animateLogo() {
+function animateLogoOnHover() {
 
   const playerContainers = document.querySelectorAll(".lottie-animation");
 
@@ -16,6 +16,31 @@ function animateLogo() {
       player.play();
     });
   });
+}
+
+function animateLogoMobile() {
+  const playerContainers = document.querySelectorAll(".lottie-animation");
+
+  setInterval(() => {
+    const checkScreenSize = window.matchMedia("(max-width: 479px)");
+    if (checkScreenSize.matches) {
+      playerContainers.forEach(container => {
+        const player = container.querySelector("dotlottie-player")
+        console.log("hey");
+        player.setDirection(1);
+        player.play();
+        setTimeout(() => {
+          player.setDirection(-1);
+          player.play();
+        }, 3000);
+      });
+    }
+  }, 10000)
+}
+
+function animateLogo() {
+  animateLogoOnHover();
+  animateLogoMobile();
 }
 
 export default animateLogo
